83f1753a8e9082a01aa317ae72cd9f0d4549992d
Sami Nafis <78338492+saminafisdev@users.noreply.github.com>
2025-09-02 16:56:08 +0600

    refactor URL patterns for Djoser authentication to use path instead of re_path

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -23,6 +23,6 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("social.urls")),
-    re_path(r"^auth/", include("djoser.urls")),
-    re_path(r"^auth/", include("djoser.urls.jwt")),
+    path("api/auth/", include("djoser.urls")),
+    path("api/auth/", include("djoser.urls.jwt")),
 ] + debug_toolbar_urls()

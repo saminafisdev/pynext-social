352263d5e83fdea10ee652cef8c35970987954fe
Sami Nafis <78338492+saminafisdev@users.noreply.github.com>
2025-09-02 16:54:10 +0600

    add custom User admin for app:core

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,4 +1,65 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
 from .models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class CustomUserAdmin(BaseUserAdmin):
+    # add_form = CustomUserCreationForm
+    # form = CustomUserChangeForm
+
+    # display in list view
+    list_display = (
+        "first_name",
+        "last_name",
+        "email",
+        "username",
+        "is_staff",
+        "is_active",
+    )
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+
+    # searchable fields
+    search_fields = ("email", "username", "first_name", "last_name")
+    ordering = ("email",)
+
+    # field sections on change view
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    # field sections on add view
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "username",
+                    "first_name",
+                    "last_name",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                ),
+            },
+        ),
+    )

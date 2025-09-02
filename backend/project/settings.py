afb6eff1218baa32e4f7353f473f776618b6ca47
Sami Nafis <78338492+saminafisdev@users.noreply.github.com>
2025-09-02 14:48:18 +0600

    create app core and add customer User model

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "core",
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "core.User"

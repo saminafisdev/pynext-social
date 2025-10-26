75777f3dfda99f1d82c804e9a7aa6cdb84ea997e
Sami Nafis <78338492+saminafisdev@users.noreply.github.com>
2025-10-26 20:46:03 +0600

    chore: add build script for backend setup and configure static file handling in Django settings

--- backend/project/django/base.py
+++ backend/project/django/base.py
@@ -35,6 +35,7 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -122,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 

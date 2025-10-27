2a4faafc263383d919fc0bbb5ae71553925efa55
Sami Nafis <78338492+saminafisdev@users.noreply.github.com>
2025-10-28 03:34:54 +0600

    debug websocket handshake rejetion issue

--- backend/project/asgi.py
+++ backend/project/asgi.py
@@ -2,9 +2,8 @@ import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.django.local")
 
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
+from channels.security.websocket import AllowedHostsOriginValidator, OriginValidator
 from django.core.asgi import get_asgi_application
 
 # Initialize Django ASGI application early to ensure the AppRegistry
@@ -15,11 +14,17 @@ django_asgi_app = get_asgi_application()
 from chat.auth_middleware import JWTAuthMiddlewareStack
 from chat.routing import websocket_urlpatterns
 
+allowed_origins = [
+    "https://djsocial.onrender.com",  # backend domain
+    "https://djsocial-omega.vercel.app",  # frontend domain
+]
+
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(
-            JWTAuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        "websocket": OriginValidator(
+            JWTAuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+            allowed_origins=allowed_origins,
         ),
     }
 )

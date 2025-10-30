fcba29fcde62c54875b774ca988946677c90549b
Sami Nafis <78338492+saminafisdev@users.noreply.github.com>
2025-10-30 14:11:13 +0600

    REST framework uses JSON renderer in production

--- backend/project/django/production.py
+++ backend/project/django/production.py
@@ -4,6 +4,9 @@ from project.env import env
 
 DEBUG = env.bool("DEBUG", default=False)
 
+# Disbale browsable API in production
+REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = ("rest_framework.renderers.JSONRenderer",)
+
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
 
 CSRF_TRUSTED_ORIGINS = env("CSRF_TRUSTED_ORIGINS", default=[])

9270bff2b88c5e66b9fe73d2db4d0de0dd636eb3
Sami Nafis <78338492+saminafisdev@users.noreply.github.com>
2025-10-31 02:28:22 +0600

    feat: add index.html template and logo.svg for homepage, configure static files

--- backend/project/django/base.py
+++ backend/project/django/base.py
@@ -55,7 +55,9 @@ INTERNAL_IPS = [
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / "templates",
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -126,6 +128,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]  # your dev static folder
 MEDIA_URL = "media/"
 # MEDIA_ROOT = BASE_DIR / "media"
 
--- backend/project/urls.py
+++ backend/project/urls.py
@@ -20,9 +20,11 @@ from django.urls import include, path
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
+    path("", TemplateView.as_view(template_name="index.html"), name="home"),
     path("admin/", admin.site.urls),
     path("api/", include("social.urls")),
     path("api/", include("chat.urls")),

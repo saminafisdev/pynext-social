c5e02caf3a0c51b1fe90a9c9e50a563daa82d537
Sami Nafis <78338492+saminafisdev@users.noreply.github.com>
2025-10-30 16:35:23 +0600

    Revert "feat: add search functionality for profiles with full name concatenation"
    This reverts commit 4d63dea5e964c2dbb308f335ab7babb66d12b379.

--- backend/social/urls.py
+++ backend/social/urls.py
@@ -1,20 +1,24 @@
 from django.urls import include, path
 from rest_framework_nested import routers
 
-from . import views
+from .views import (
+    BookmarkViewSet,
+    CommentViewSet,
+    PostLikeViewSet,
+    PostViewSet,
+    ProfileViewSet,
+)
 
 router = routers.SimpleRouter()
-router.register(r"profiles", views.ProfileViewSet, basename="profile")
-router.register(r"posts", views.PostViewSet, basename="post")
-router.register(r"bookmarks", views.BookmarkViewSet, basename="bookmark")
-router.register(r"friends", views.FriendViewSet, basename="friend")
+router.register(r"profiles", ProfileViewSet, basename="profile")
+router.register(r"posts", PostViewSet, basename="post")
+router.register(r"bookmarks", BookmarkViewSet, basename="bookmark")
 
 posts_router = routers.NestedSimpleRouter(router, r"posts", lookup="post")
-posts_router.register(r"comments", views.CommentViewSet, basename="comment")
-posts_router.register(r"likes", views.PostLikeViewSet, basename="like")
+posts_router.register(r"comments", CommentViewSet, basename="comment")
+posts_router.register(r"likes", PostLikeViewSet, basename="like")
 
 urlpatterns = [
     path("", include(router.urls)),
     path("", include(posts_router.urls)),
-    path("search/", views.search, name="profile-search"),
 ]
